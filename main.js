2e75793d26563a29d515a1525e448b15edeaec61
ben-abb <100.benabbott@gmail.com>
2025-04-20 14:39:19 -0400

    fixed import

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //import * as THREE from 'three';
 //import $ from "jquery";
+import * as THREE from "https://unpkg.com/three@0.175.0/build/three.module.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
